88bdea430007346a8db9fb27bee1a72c48572fe1
malac123 <merisskakic0@gmail.com>
2026-02-11 23:06:06 +0100

    Wrapped Dropdownmenu and Settings Icon into Box for style reasons
    Brought back trash icon instead of "x"

--- app/src/main/java/com/jazo/quicknotes/MainActivity.kt
+++ app/src/main/java/com/jazo/quicknotes/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.jazo.quicknotes
 
 // imports for date formatting
-import java.util.Date
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,55 +18,47 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.jazo.quicknotes.ui.theme.QuickNotesTheme
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.Card
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.foundation.background
-
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.AlertDialog
-
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Red
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 
 @Composable
@@ -121,28 +109,34 @@ fun NoteScreen()  {
             }
 
 
-            IconButton(onClick = {showSettings = true}) {      // TODO: add settings menu
-                Icon(
-                    imageVector = Icons.Default.Settings,
-                    contentDescription = "Settings",
-                    tint = Color.White
+            Box(modifier = Modifier.wrapContentSize(Alignment.TopEnd)) {
+                IconButton(onClick = {showSettings = true}) {      // Done
+                    Icon(
+                        imageVector = Icons.Default.Settings,
+                        contentDescription = "Settings",
+                        tint = Color.White
+                    )
+                }
+
+                DropdownMenu(expanded = showSettings, onDismissRequest = {showSettings = false}, containerColor = Color(
+                    0xFF545050
                 )
+                ) {
+
+                    DropdownMenuItem(text = { Text("GitHub Page") }, onClick = {showSettings = false})       // Says dark mode but doesn't do anything :(
+                    Divider(color = Color.White, modifier = Modifier.padding(vertical = 8.dp))
+                    DropdownMenuItem(text = {Text("Clear all Notes")}, onClick = {
+                        showSettings = false
+                        notes.forEach { viewModel.delete(it) }
+                    })
+                    Divider(color = Color.White, modifier = Modifier.padding(vertical = 8.dp))
+                    DropdownMenuItem(text = {Text("About")}, onClick = {
+                        showSettings = false
+                        showDialog = true
+                    })
+                }
             }
 
-            DropdownMenu(expanded = showSettings, onDismissRequest = {showSettings = false}) {
-
-                DropdownMenuItem(text = { Text("Dark Mode") }, onClick = {showSettings = false})       // Says dark mode but doesn't do anything :(
-
-                DropdownMenuItem(text = {Text("Clear all Notes")}, onClick = {
-                    showSettings = false
-                    notes.forEach { viewModel.delete(it) }
-                })
-
-                DropdownMenuItem(text = {Text("About")}, onClick = {
-                    showSettings = false
-                    showDialog = true
-                })
-            }
 
 
         }
@@ -229,8 +223,19 @@ fun NoteScreen()  {
 
                             Spacer(modifier = Modifier.width(16.dp))
 
-                            IconButton(onClick = { viewModel.delete(note) }) {
-                                Text("×", fontSize = 24.sp, color = Color.White)
+                            IconButton(
+                                onClick = { viewModel.delete(note) },
+                                modifier = Modifier
+                                    .size(32.dp)
+                                    .border(1.dp, Color.Gray.copy(alpha = 0.3f), CircleShape)
+                                    .padding(4.dp)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Delete,
+                                    contentDescription = "Delete",
+                                    tint = Color.White,
+                                    modifier = Modifier.size(18.dp)
+                                )
                             }
                         }
                     }
